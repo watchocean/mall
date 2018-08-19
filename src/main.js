d4ee3a722a25038f73ff40c69a5b65c0f55dd837
alex <zhan572637307@gmail.com>
2018-08-19 20:05:06 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,46 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueLazyLoad from 'vue-lazyload'
+import infiniteScorll from 'vue-infinite-scroll'
+import {currency} from './util/currency'
+import Vuex from 'vuex'
+
+Vue.config.productionTip = false
+Vue.use(infiniteScorll);
+Vue.use(Vuex);
+
+Vue.use(VueLazyLoad, {
+	loading: "/static/loading-svg/loading-bars.svg"
+});
+
+Vue.filter('currency',currency);
+
+const store = new Vuex.Store({
+	state: {
+		nickName: '',
+		cartCount: 0
+	},
+	mutations: {
+		updateUserInfo(state,nickName){
+			state.nickName = nickName;
+		},
+		updateCartCount(state,cartCount){
+			state.cartCount += cartCount;
+		},
+		initCartCount(state,cartCount){
+			state.cartCount = cartCount;
+		}
+	}
+});
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  store,
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,229 @@
+<template>
+	<div>
+		<nav-header></nav-header>
+		<nav-bread>
+			<span>Goods</span>
+		</nav-bread>
+		<div class="accessory-result-page accessory-page">
+		  	<div class="container">
+		    	<div class="filter-nav">
+		      		<span class="sortby">Sort by:</span>
+		      		<a href="javascript:void(0)" class="default cur">Default</a>
+		      		<a href="javascript:void(0)" class="price" @click="sortGoods">Price <svg class="icon icon-arrow-short" :class="{'sort-up':!sortFlag}"><use xlink:href="#icon-arrow-short"></use></svg></a>
+		      		<a href="javascript:void(0)" class="filterby stopPop" @click="showFilterBy">Filter by</a>
+		    	</div>
+		    	<div class="accessory-result">
+		      		<!-- filter -->
+		      		<div class="filter stopPop" id="filter" :class="{'filterby-show':filterBy}">
+		        		<dl class="filter-price">
+		          			<dt>Price:</dt>
+		          			<dd><a href="javascript:void(0)" :class="{cur:priceChecked==='all'}" @click="priceChecked='all'">All</a></dd>
+		          			<dd v-for="(price, index) in priceFilter">
+		            		<a href="javascript:void(0)" @click="setPriceFilter(index)" :class="{cur:priceChecked===index}">{{price.startPrice}} - {{price.endPrice}}</a>
+		          			</dd>
+		        		</dl>
+		      		</div>
+
+		      		<!-- search result accessories list -->
+		      		<div class="accessory-list-wrap">
+		        		<div class="accessory-list col-4">
+		          			<ul>
+		            			<li class="item" v-for="item in goodsList">
+		              				<div class="pic">
+		                				<a href="#"><img v-lazy="'../static/'+item.productImage" alt=""></a>
+		              				</div>
+		              				<div class="main">
+		                				<div class="name">{{item.productName}}</div>
+		                				<div class="price">{{item.salePrice}}</div>
+		                				<div class="btn-area">
+		                  					<a href="javascript:;" class="btn btn--m" @click="addCart(item.productId)">加入购物车</a>
+		                				</div>
+		              				</div>
+		            			</li>
+		          			</ul>
+		          			<div class="load-more" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="30">
+  								加载中...
+							</div>
+		        		</div>
+		      		</div>
+		    	</div>
+		  	</div>
+		</div>
+		<modal v-bind:mdShow="mdShow"  v-on:close="closeModal">
+      		<p slot="message">
+        		请先登录，否则无法加入到购物车中！
+      		</p>
+        	<div slot="btnGroup">
+        		<a class="btn btn--m" href="javascript:;" @click="mdShow = false">关闭</a>
+        	</div>
+    	</modal>
+    	<modal v-bind:mdShow="mdShowCart" v-on:close="closeModal">
+        	<p slot="message">
+        		<svg class="icon-status-ok">
+          		<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-status-ok"></use>
+        		</svg>
+        		<span>加入购物车成功</span>
+      		</p>
+      		<div slot="btnGroup">
+       			<a class="btn btn--m" href="javascript:;" @click="mdShowCart = false">继续购物</a>
+        		<router-link class="btn btn--m" to="/cart" >查看购物车</router-link>
+      		</div>
+    	</modal>
+		<div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
+		<nav-footer></nav-footer>
+	</div>
+</template>
+<script>
+	import './../assets/css/base.css';
+	import "../assets/css/product.css";
+	import "../assets/css/login.css";
+	import NavHeader from '../components/NavHeader.vue';
+	import NavBread from '../components/NavBread.vue';
+	import NavFooter from '../components/NavFooter.vue';
+	import Modal from '../components/Modal'
+	import axios from 'axios';
+	export default {
+		data() {
+			return {
+				goodsList: [],
+				priceFilter: [
+					{
+						startPrice: '0.00',
+						endPrice: '500.00'
+					},
+					{
+						startPrice: '500.00',
+						endPrice: '1000.00'
+					},
+					{
+						startPrice: '1000.00',
+						endPrice: '2000.00'
+					}
+				],
+				priceChecked: 'all',
+				filterBy: false,
+				overLayFlag: false,
+				sortFlag: true,
+				page: 1,
+				pageSize: 8,
+				busy: true,
+				mdShow: false,
+      			mdShowCart:false
+			};
+		},
+		components: {
+			NavHeader,
+			NavBread,
+			NavFooter,
+			Modal
+		},
+		mounted() {
+			this.getGoodsList();
+		},
+		methods: {
+			getGoodsList(flag) {
+				let param = {
+					page: this.page,
+					pageSize: this.pageSize,
+					sort: this.sortFlag?1:-1,
+					priceLevel: this.priceChecked
+				};
+				axios.get('/goods/list', {params:param}).then((result) => {
+					let res = result.data;
+					if(res.status == "0"){
+						if(flag){
+							this.goodsList = this.goodsList.concat(res.result.list);
+
+							if(res.result.count == 0){
+								this.busy = true;
+							}else{
+								this.busy = false;
+							}
+						}else{
+							this.goodsList = res.result.list;
+							this.busy = false;
+						}
+					}else{
+						this.goodsList = [];
+					}	
+				});
+			},
+			showFilterBy() {
+				this.filterBy = true;
+				this.overLayFlag = true;
+			},
+			setPriceFilter(index) {
+				this.priceChecked = index;
+				this.closePop();
+				this.page = 1;
+				this.getGoodsList();
+			},
+			closePop() {
+				this.filterBy = false;
+				this.overLayFlag = false;
+			},
+			sortGoods(){
+				this.sortFlag = !this.sortFlag;
+				this.page = 1;
+				this.getGoodsList();
+			},
+			loadMore() {
+				this.busy = true;
+				setTimeout(() => {
+					this.page++;
+					this.getGoodsList(true);
+				},500);
+			},
+			addCart(productId){
+				axios.post('/goods/addCart',{productId:productId}).then((res) => {
+					if(res.status === 200){
+						this.mdShowCart = true;
+						this.$store.commit("updateCartCount",1);
+					}else{
+						console.log(res.status);
+						this.mdShow = true;
+					}
+				});
+			},
+			closeModal() {
+      			this.mdShow = false;
+      			this.mdShowCart = false;
+    		}
+		}
+	};
+</script>
+<style scoped>
+	.list-wrap ul:after {
+	 	clear: both;
+	  	content: '';
+	  	height: 0;
+	  	display: block;
+	  	visibility: hidden;
+	}
+
+	.load-more {
+	  height: 100px;
+	  line-height: 100px;
+	  text-align: center;
+	}
+
+	.sort-up {
+	  transform: rotate(180deg);
+	  transition: all .3s ease-out;
+	}
+
+	.icon-arrow-short {
+	  width: 11px;
+	  height: 11px;
+	  transition: all .3s ease-out;
+	}
+
+	.btn:hover {
+	  background-color: #ffe5e6;
+	  transtion: all .3s ease-out;
+	}
+	.sort-up {
+		transform: rotate(180deg);
+		transition: all 0.3s ease-out;
+	}
+</style>
